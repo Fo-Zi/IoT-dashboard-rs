020c9e8d17eb9330e76c1fc47557e54302432e74
root <root@FZ-Desktop>
2024-12-01 20:47:08 -0300

    Playing around with Tokio and rumqtt
    Next step would be to add Slint and have a very basic interface to show data

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rumqttc::{AsyncClient, Event, MqttOptions, Packet, QoS};
+use std::error::Error;
+use std::time::Duration;
+use tokio::{sync::mpsc, task};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize MQTT client
+    let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    client
+        .subscribe("test/topic", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    // Create a channel to transfer messages between threads
+    let (tx, mut rx) = mpsc::channel(100);
+
+    // Spawn a task to handle message receiving
+    task::spawn(async move {
+        while let Ok(event) = eventloop.poll().await {
+            if let Event::Incoming(Packet::Publish(publish)) = event {
+                let message = String::from_utf8_lossy(&publish.payload).to_string();
+                if (tx.send((publish.topic.clone(), message)).await).is_err() {
+                    println!("Receiver dropped. Exiting message handler.");
+                    break;
+                }
+            }
+        }
+    });
+
+    // Main thread processing received messages
+    while let Some(msg) = rx.recv().await {
+        println!("Received message - Topic:{} - Message: {}", msg.0, msg.1);
+    }
+
+    Ok(())
+}
